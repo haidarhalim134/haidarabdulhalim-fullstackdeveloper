3848d7176eca677772d550d07d0e853b913dd0cf
haidarhalim134 <haidarah134@gmail.com>
2026-09-21 21:24:33 +0700

    setup project for local development

--- api/index.ts
+++ api/index.ts
@@ -8,4 +8,10 @@ app.get('/api/healthcheck', (req: Request, res: Response) => {
   res.status(200).json({ status: 'ok', message: 'Express server running on Vercel!' });
 });
 
+// for local development
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.BACKEND_PORT || 5000;
+  app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+}
+
 export default app;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // for local development
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', 
+        changeOrigin: true,
+      },
+    },
+  },
 })
