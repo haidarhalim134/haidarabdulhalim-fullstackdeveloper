d63bf886795b6ed88c8b3af8c3a1ac85cc9bcb97
haidarhalim134 <haidarah134@gmail.com>
2026-09-22 15:42:46 +0700

    fix shadcn setup

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,14 +1,23 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-],
-
+  ],
+  resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './'),
+      },
+    },
   // for local development
   server: {
     proxy: {
